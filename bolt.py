996fddcdf8a336a2b2f18112a1df7a006d77bc0b
Ruben <111981761+R7b3nv0k@users.noreply.github.com>
2024-01-08 22:40:40 +0100

    Update bolt.py

--- bolt.py
+++ bolt.py
@@ -1,22 +1,15 @@
-import yfinance as yf
+import sqlite3
 import streamlit as st
-import pandas as pd
 
-st.write("""
-# Simple Stock Price App
-
-Shown are the stock closing price and volume of Google!
-
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open    High    Low    Close    Volume    Dividends    Stock Splits
-
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+def create_database():
+    
+    conn = sqlite3.connect('customers.db')
+    c = conn.cursor()
+    c.execute("""
+    SELECT name FROM sqlite_master WHERE type='table' AND name='customers'
+    """)
+    if not c.fetchone():
+        c.execute('''CREATE TABLE customers
+                     (name text, address text, phone text)''')
+        conn.commit()
+    conn.close()
